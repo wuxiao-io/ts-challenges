60911f428b149265c735b6d9a45836ad345b31da
weisuoke <530264127@qq.com>
2023-02-06 18:30:28 +0800

    feat: 🎸 EndsWith

---
+++ src/types/EndsWidth.ts
@@ -0,0 +1,16 @@
+// Impl1
+type EndsWidth<T extends string, U extends string> = T extends `${infer R}${U}` ? true : false;
+
+// Impl2
+type RemoveFirst<T extends string> = T extends `${infer first}${infer rest}` ? rest : ''
+
+type EndsWith2<T extends string, U extends string> = U extends '' ? true :
+  T extends U ? true
+  : T extends `${infer first}${infer rest}` ? U extends `${RemoveFirst<T>}`
+  ? true : EndsWith2<RemoveFirst<T>, U>
+  : false;
+
+export {
+  EndsWidth,
+  EndsWith2
+}
